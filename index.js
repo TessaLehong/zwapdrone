8e58845b97fdf67668847b64799ede935c8509a9
TessaLehong <136630270+TessaLehong@users.noreply.github.com>
2024-10-17 21:28:24 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const drone = document.querySelector('.drone');
+
+// No need for flash-related functions anymore
+
+// Trigger any other actions if needed when the drone animation ends
+drone.addEventListener('animationiteration', () => {
+    // You can add other actions here if needed
+});
